d5a1e9f31ffe2984dc703b36d2ec48389df64f82
Mark Sinakaev <Sinakaev@lanit.ru>
2025-06-15 01:14:53 +0300

    refactor: split Node-only constants into separate file

---
+++ src/build-constants.ts
@@ -0,0 +1,3 @@
+import path from 'node:path'
+
+export const MODULE_ENTRYPOINT = path.resolve(process.cwd(), 'src/index.tsx')
--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,5 @@
-import path from 'node:path'
-
 export const NPM_REGISTRY = 'https://registry.npmjs.org/'
 export const DEV_DIR = '.dev'
-export const MODULE_ENTRYPOINT = path.resolve(process.cwd(), 'src/index.tsx')
 
 export const EXTERNAL_DEPS = [
 	'react',
